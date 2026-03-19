d1497bd4eff15375baf6b34e7dcf072c1f01da9e
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-03-19 17:42:15 +0100

    chore: migrate client to effect v4-beta

--- apps/client/src/lib/atom.ts
+++ apps/client/src/lib/atom.ts
@@ -1,12 +1,12 @@
-import { DevTools } from "@effect/experimental";
-import { Atom } from "@effect-atom/atom-react";
 import { Layer } from "effect";
+import { DevTools } from "effect/unstable/devtools";
+import { Atom } from "effect/unstable/reactivity";
 import { RpcClient } from "./rpc-client";
 
 const ENABLE_DEVTOOLS = import.meta.env.VITE_ENABLE_DEVTOOLS === "true";
 
 export const runtime = Atom.runtime(
-  RpcClient.Default.pipe(
+  RpcClient.layer.pipe(
     Layer.provideMerge(ENABLE_DEVTOOLS ? DevTools.layer() : Layer.empty),
   ),
 );
